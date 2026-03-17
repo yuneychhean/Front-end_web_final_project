32d77da09f69e3a9bd25f0792a6a9c5a8e77af34
yun eychhean <your-github-email@example.com>
2026-03-17 16:46:57 +0700

    Navbar

--- Movio-website/src/components/Button.jsx
+++ Movio-website/src/components/Button.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+function Button({ className = "", size = "dafault", children }) {
+  const baseClasses =
+    "relative overflow-hidden rounded-full font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-[#20b2a6] border border-[#18E3B5] text-[#18E3B5] hover:bg-[#18E3B5] hover:text-[#F5F5F5]/70 shadow-lg shadow-[#202ba6]/25";
+  const sizeClasses = {
+    sm: "px-3 py-1 text-sm",
+    md: "px-5 py-2 text-base",
+    lg: "px-6 py-3 text-lg",
+  };
 
-const Button = () => {
+  const classes = `${baseClasses} ${sizeClasses[size] || ""} ${className || "" }`;
+  
   return (
-    <div>Button</div>
-  )
+  <button className={classes}>
+    <span className=" relative flex items-center justify-center gap-2">
+      {children}
+    </span>
+  </button>);
 }
 
 export default Button;
--- Movio-website/src/layouts/Navbar.jsx
+++ Movio-website/src/layouts/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import MovioLogo from "../assets/MovioLogo.png";
-import { Menu, X } from "lucide-react";
+import { Menu, X, Search, Heart } from "lucide-react";
+import Button from "../components/Button.jsx"
 
 export const Navbar = () => {
   const [isMobileMenu, setIsMobileMenu] = useState(false);
@@ -13,27 +14,43 @@ export const Navbar = () => {
   ];
 
   return (
-    <header>
-      <nav className="flex items-center justify-between p-4">
+    <header className="fixed top-0 right-0 left-0 bg-transparent py-3 border border-b-[#18E3B5]">
+      <nav className="container mx-auto px-3 flex items-center justify-between">
         {/* Logo */}
         <img src={MovioLogo} alt="Movio Logo" className="w-32" />
 
         {/* Links */}
-        <ul className="hidden md:flex gap-6">
+        <ul className="hidden md:flex gap-3 items-center">
           {navLinks.map((link, index) => (
-            <li key={index}>
+            <li
+              key={index}
+              className="text-sm text-[#F5F5F5] hover:text-[#18E3B4] rounded-full hover:bg-[#2f6078]/30 font-bold px-3 py-2"
+            >
               <a href={link.href}>{link.label}</a>
             </li>
           ))}
         </ul>
-
+        <div className="hidden md:flex justify-center items-center gap-4 ">
+          <span className=" flex items-center justify-center gap-2">
+            <Search />
+            <Heart />
+          </span>
+          <div className="hidden md:block ">
+            <Button size="sm">Login</Button>
+          </div>
+        </div>
         {/* Mobile Button */}
-        <button
-          className="md:hidden p-2 text-[#f0f2f5] cursor-pointer"
-          onClick={() => setIsMobileMenu((prev) => !prev)}
-        >
-          {isMobileMenu ? <X size={24} /> : <Menu size={24} />}
-        </button>
+        <div className=" flex justify-center items-center md:hidden gap-2">
+          <Search />
+          <Heart />
+          <button
+            className="md:hidden p-2 text-[#f0f2f5] cursor-pointer"
+            onClick={() => setIsMobileMenu((prev) => !prev)}
+          >
+            {isMobileMenu ? <X size={24} /> : <Menu size={24} />}
+          </button>
+          
+        </div>
       </nav>
 
       {/* Mobile Menu */}
