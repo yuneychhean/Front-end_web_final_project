616fc3cd1accfdb48b1ce4b1321754682ac1e1c5
Chay-SengHap <chaysenghabb@gmail.com>
2026-03-30 22:55:19 +0700

    modify popular layout

--- Movio-website/src/sections/Popular.jsx
+++ Movio-website/src/sections/Popular.jsx
@@ -3,7 +3,7 @@ import Title from '../components/Title';
 import DisplayPopularMovie from '../components/DisplayPopularMovie';
 const Popular = () => {
   return (
-    <div className='pt-18 sm:pt-20 sm:pb-20'>
+    <div className='pt-18 sm:pt-20 sm:pb-20 px-4 sm:px-10'>
       <Title title={'Popular Movie'}/>
       <DisplayPopularMovie />
     </div>
