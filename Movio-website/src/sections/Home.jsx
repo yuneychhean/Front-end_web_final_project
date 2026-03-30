2e2f345119adbc16b4a9725c21cc09ddbd302a53
Roth Sorayuth <sorayuthroth@gmail.com>
2026-03-30 15:12:42 +0700

    adjust popular in main

--- Movio-website/src/sections/Home.jsx
+++ Movio-website/src/sections/Home.jsx
@@ -7,21 +7,26 @@ import axios from "axios";
 import DisplayAllMovie from "../components/DisplayAllMovie";
 
 const Home = () => {
-  const [popular, setPopular] = useState([]);
+  const [PopularMovies, setPopularMovies] = useState([]);
   
   useEffect(() => {
-    axios.get('/data/popular.json')
-      .then(res => setPopular(res.data))
-      .catch(e => console.log(e))
+    axios.get('/data/movie.json')
+       .then(res => {
+        const filtered = res.data.filter(item => item.rating >= 8.5);
+        setPopularMovies(filtered);
+      })
+      .catch(e => {
+        console.log(e);
+      })
   }, []);
 
   return (
     <div className="px-2 sm:px-4">
       <Carousel />
       <Title title={"What's Popular"}/>
-      <MovieGrid movies={popular} />
+      <MovieGrid movies={PopularMovies} />
       <Title title={"Dong Hua"}/>
-      <MovieGrid movies={popular} />
+      <MovieGrid movies={PopularMovies} />
       
     </div>
   );
